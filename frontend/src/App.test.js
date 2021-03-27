edf884aa08e456ffed58130bb4846af188bb04f9
Gabe Abud <gabriel.jabud@gmail.com>
2021-03-27 14:42:26 -0700

    add frontend tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+test('example test', () => {
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
