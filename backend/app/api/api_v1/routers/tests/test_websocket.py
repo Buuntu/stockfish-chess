2dff0df80868a3050dd0bdc305dcd2c1cf49b014
Gabe Abud <gabriel.jabud@gmail.com>
2021-03-28 19:27:59 -0700

    remove hanging websocket test

--- backend/app/api/api_v1/routers/tests/test_websocket.py
+++
@@ -1,4 +0,0 @@
-def test_websocket(client):
-    with client.websocket_connect("/api/ws/lobby") as websocket:
-        data = websocket.receive_json()
-        assert data == {"msg": "Hello WebSocket"}
